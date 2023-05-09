7d6316b65d3e329872b1025577f8f515f51e033e
Binh <tranhuybinh00@gmail.com>
2023-05-09 14:18:39 +0700

    add app unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { LANGUAGES } from '@constant';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { AppRouitngModule } from './app-rouitng.module';
 
 describe('AppComponent', () => {
+  function httpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+    return new TranslateHttpLoader(http, '../../assets/i18n/', '.json');
+  }
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [ AppComponent ],
+      imports: [
+        HttpClientModule,
+        AppRouitngModule,
+        TranslateModule.forRoot({
+          defaultLanguage: LANGUAGES.English,
+          loader: {
+            provide: TranslateLoader,
+            useFactory: httpLoaderFactory,
+            deps: [HttpClient]
+          }
+        }),
+      ]
     }).compileComponents();
   });
 
